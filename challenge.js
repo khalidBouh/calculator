ce9886429a7347102eeea68a8885df23c4642e00
khalidBouh <khaalidm26@gmail.com>
2023-08-01 02:04:48 +0300

    Calculator Answer

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator (num1, num2, operator){
+    return num1 , num2 , operator
+}
+if (operator === "*"){
+    console.log(num1+num2===operator)
+} else if (operator === "-"){
+    console.log(num1-num2)
+} else if (operator === "*"){
+    console.log(num1*num2)
+}else if (operator === "/"){
+    console.log(num1/num2)
+}
+calculator(1, 2 , "+")
+calculator(1, 2 , "-")
+calculator(1, 2 , "/")
